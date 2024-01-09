2bc014105369f8cd09dff7daa578c7f9f965ab63
manhtdd <manh.td120901@gmail.com>
2024-01-09 22:48:53 +0700

    Update scripts

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 
 python3 main.py \
     -data_path /data/gpfs/projects/punim1928/RISE/JITDP/crawling/truncated_tan_dataset \
-    -output_path .
+    -output_path /data/gpfs/projects/punim1928/RISE/JITDP/crawling
