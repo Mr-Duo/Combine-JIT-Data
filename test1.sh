80f7bb28a1488920eaf962597ee9844a2de83c40
manhtdd <manh.td120901@gmail.com>
2024-01-10 09:27:07 +0700

    Update script

--- test1.sh
+++ test1.sh
@@ -5,5 +5,5 @@ rm -rf logs
 git pull
 
 python3 main1.py \
-    -data_path /data/gpfs/projects/punim1928/RISE/JITDP/crawling/truncated_tan_dataset \
+    -data_path /data/gpfs/projects/punim1928/RISE/JITDP/crawling/single-lang \
     -output_path /data/gpfs/projects/punim1928/RISE/JITDP/crawling
