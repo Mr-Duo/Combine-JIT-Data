34047103fca896971e166aff6aa7c702ed5e431f
manhtdd <manh.td120901@gmail.com>
2024-01-10 16:22:37 +0700

    Update script

--- main2.py
+++ main2.py
@@ -13,22 +13,31 @@ def main():
     args = read_args()
     single_path = args.single_path
     cross_path = args.cross_path
-    pathes = [single_path, cross_path]
     output_path = f"{args.output_path}/cross-lang-preprocessed"
 
     if not os.path.exists(output_path):
         os.mkdir(output_path)
 
-    for path in pathes:
-        for language in os.listdir(path):
-            language_path = f"{path}/{language}"
+    for language in os.listdir(single_path):
+        language_path = f"{single_path}/{language}"
 
-            folders = os.listdir(language_path)
-            commits_path, features_path = folders[0], folders[1]
-            commits_path, features_path = f"{language_path}/{commits_path}", f"{language_path}/{features_path}"
+        folders = os.listdir(language_path)
+        commits_path, features_path = folders[0], folders[1]
+        commits_path, features_path = f"{language_path}/{commits_path}", f"{language_path}/{features_path}"
 
-            for file in os.listdir(commits_path):
+        for file in os.listdir(commits_path):
+            if 'part_5' in file:
                 logger(file)
+
+    for language in os.listdir(cross_path):
+        language_path = f"{cross_path}/{language}"
+
+        folders = os.listdir(language_path)
+        commits_path, features_path = folders[0], folders[1]
+        commits_path, features_path = f"{language_path}/{commits_path}", f"{language_path}/{features_path}"
+
+        for file in os.listdir(commits_path):
+            logger(file)
 
 if __name__ == "__main__":
     main()
