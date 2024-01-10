a22963be926206159aa8c24b6ca3db09d02e7717
manhtdd <manh.td120901@gmail.com>
2024-01-10 16:17:38 +0700

    Update script

--- main2.py
+++ main2.py
@@ -4,26 +4,30 @@ from dict import create_dict
 
 def read_args():
     parser = argparse.ArgumentParser(description='Process JSON data.')
-    parser.add_argument('-data_path')
+    parser.add_argument('-single_path')
+    parser.add_argument('-cross_path')
     parser.add_argument('-output_path')
     return parser.parse_args()
 
 def main():
     args = read_args()
-    data_path = args.data_path
+    single_path = args.single_path
+    cross_path = args.cross_path
+    pathes = [single_path, cross_path]
     output_path = f"{args.output_path}/cross-lang-preprocessed"
 
     if not os.path.exists(output_path):
         os.mkdir(output_path)
 
-    for language in os.listdir(data_path):
-        language_path = f"{data_path}/{language}"
+    for path in pathes:
+        for language in os.listdir(path):
+            language_path = f"{path}/{language}"
 
-        commits_path, features_path = os.listdir(language_path)
-        commits_path, features_path = f"{language_path}/{commits_path}", f"{language_path}/{features_path}"
+            commits_path, features_path = os.listdir(language_path)
+            commits_path, features_path = f"{language_path}/{commits_path}", f"{language_path}/{features_path}"
 
-        for file in os.listdir(commits_path):
-            logger(file)
+            for file in os.listdir(commits_path):
+                logger(file)
 
 if __name__ == "__main__":
     main()
--- test2.sh
+++ test2.sh
@@ -5,5 +5,6 @@ rm -rf logs
 git pull
 
 python3 main2.py \
-    -data_path /data/gpfs/projects/punim1928/RISE/JITDP/crawling/cross-lang \
+    -single_path /data/gpfs/projects/punim1928/RISE/JITDP/crawling/single-lang \
+    -cross_path /data/gpfs/projects/punim1928/RISE/JITDP/crawling/cross-lang \
     -output_path /data/gpfs/projects/punim1928/RISE/JITDP/crawling
