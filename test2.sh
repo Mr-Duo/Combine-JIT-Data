d7dfd68e1695ee33a37326bc712163bf19955b57
manhtdd <manh.td120901@gmail.com>
2024-01-10 16:08:57 +0700

    Update script

--- test2.sh
+++ test2.sh
@@ -4,6 +4,6 @@ rm -rf logs
 
 git pull
 
-python3 main.py \
+python3 main2.py \
     -data_path /data/gpfs/projects/punim1928/RISE/JITDP/crawling/cross-lang \
     -output_path /data/gpfs/projects/punim1928/RISE/JITDP/crawling
